48716aafbc70213523537ec69dc6d3299b2dbea1
Surya Duggirala <suryad96@berkeley.edu>
2017-06-09 16:33:49 -0700

    finished a functioning shell script

--- CommandLineVersion/commandPrompt.sh
+++ CommandLineVersion/commandPrompt.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
+
 while true; do 
     read -p "Analytics: " cmd
-    java Main $cmd
+    echo $cmd
+    if [ "$cmd" = "exit" ] 
+    then 
+        exit 2
+        clear
+    else
+        java Main $cmd
+    fi
 done
 
